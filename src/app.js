1404ebfa137719702688e61636977c31280c06bb
anwaar95m <manwaarh619@gmail.com>
2021-02-12 23:19:23 +0500

    updated app.js

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ const geocode = require("./utils/geocode");
 const forecast = require("./utils/forecast");
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 //Paths
 const publicDirectoryPath = path.join(__dirname, "../public");
@@ -76,7 +77,6 @@ app.get("*", (req, res) => {
   });
 });
 
-const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
